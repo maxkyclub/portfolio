0d62ebc5c1485f09985362769d79635d835b5ad6
Thanatorn <thanatorn@amity.co>
2021-10-04 16:35:42 +0700

    commit first version

---
+++ components/About.js
@@ -0,0 +1,40 @@
+import React from "react"
+import Image from "next/image"
+import profile from "../public/profile.jpeg"
+
+const About = () => {
+    return (
+        <div className="container px-4 mx-auto">
+            <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
+                <div className="lg:px-4 lg:mt-12 ">
+                    <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
+                        Looking for a software engineer?
+                    </h1>
+                    <div className="mt-6 text-gray-800 dark:text-white">
+                        <p className="mb-4">
+                            Hello! My name is Thanatorn Boonnak. I am a Thailand-based software engineer.
+                            I have a diverse set of skills, ranging from people to technical. Web development. 
+                            Experienced in elearning, healthcare, insurance, hospitality fields.
+                        </p>
+
+                        <p>
+                            NodeJS, PHP, ExpressJS, Laravel, HTML, CSS, JavaScript, Tailwind, AWS, GCP
+                        </p>
+                    </div>
+                </div>
+                <div className="flex-shrink-0 lg:mt-12 lg:px-4 mb-10">
+                    <Image
+                        src={profile}
+                        alt="Profile"
+                        priority={true}
+                        className="rounded-full"
+                        width={200}
+                        height={250}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
